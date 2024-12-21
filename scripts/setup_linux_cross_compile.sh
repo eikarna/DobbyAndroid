9739ccbed081cf486d31203984f9cb293c3522d0
eikarna <nixic0@proton.me>
2024-12-21 11:25:06 +0700

    Update

--- scripts/setup_linux_cross_compile.sh
+++ scripts/setup_linux_cross_compile.sh
@@ -37,7 +37,7 @@ mkdir -p ~/opt
 cd ~/opt
 CMAKE_VERSION=3.25.2
 CMAKE_DOWNLOAD_PACKAGE=cmake-$CMAKE_VERSION-linux-x86_64
-wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_DOWNLOAD_PACKAGE.tar.gz &&
+wget -q https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_DOWNLOAD_PACKAGE.tar.gz &&
   tar -zxf $CMAKE_DOWNLOAD_PACKAGE.tar.gz >/dev/null &&
   mv $CMAKE_DOWNLOAD_PACKAGE cmake-$CMAKE_VERSION
 CMAKE_HOME=~/opt/cmake-$CMAKE_VERSION
@@ -45,7 +45,7 @@ CMAKE_HOME=~/opt/cmake-$CMAKE_VERSION
 cd ~/opt
 LLVM_VERSION=15.0.6
 LLVM_DOWNLOAD_PACKAGE=clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-18.04
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVM_DOWNLOAD_PACKAGE.tar.xz &&
+wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVM_DOWNLOAD_PACKAGE.tar.xz &&
   tar -xf $LLVM_DOWNLOAD_PACKAGE.tar.xz >/dev/null &&
   mv $LLVM_DOWNLOAD_PACKAGE llvm-$LLVM_VERSION
 LLVM_HOME=~/opt/llvm-$LLVM_VERSION
@@ -54,7 +54,7 @@ cd ~/opt
 NDK_VERSION=r25b
 NDK_DOWNLOAD_PACKAGE=android-ndk-$NDK_VERSION-linux
 NDK_DOWNLOAD_UNZIP_PACKAGE=android-ndk-$NDK_VERSION
-wget https://dl.google.com/android/repository/$NDK_DOWNLOAD_PACKAGE.zip &&
+wget -q https://dl.google.com/android/repository/$NDK_DOWNLOAD_PACKAGE.zip &&
   unzip -q $NDK_DOWNLOAD_PACKAGE.zip >/dev/null &&
   mv $NDK_DOWNLOAD_UNZIP_PACKAGE ndk-$NDK_VERSION &&
   rm $NDK_DOWNLOAD_PACKAGE.zip
--- scripts/setup_macos_cross_compile.sh
+++ scripts/setup_macos_cross_compile.sh
@@ -8,7 +8,7 @@ mkdir -p ~/opt
 cd ~/opt
 CMAKE_VERSION=3.25.2
 CMAKE_DOWNLOAD_PACKAGE=cmake-$CMAKE_VERSION-macos-universal
-wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_DOWNLOAD_PACKAGE.tar.gz &&
+wget -q https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKE_DOWNLOAD_PACKAGE.tar.gz &&
   tar -zxf $CMAKE_DOWNLOAD_PACKAGE.tar.gz >/dev/null &&
   mv $CMAKE_DOWNLOAD_PACKAGE cmake-$CMAKE_VERSION
 CMAKE_HOME=~/opt/cmake-$CMAKE_VERSION
@@ -16,7 +16,7 @@ CMAKE_HOME=~/opt/cmake-$CMAKE_VERSION
 cd ~/opt
 LLVM_VERSION=15.0.6
 LLVM_DOWNLOAD_PACKAGE=clang+llvm-$LLVM_VERSION-x86_64-apple-darwin
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVM_DOWNLOAD_PACKAGE.tar.xz &&
+wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVM_DOWNLOAD_PACKAGE.tar.xz &&
   tar -xf $LLVM_DOWNLOAD_PACKAGE.tar.xz >/dev/null &&
   mv $LLVM_DOWNLOAD_PACKAGE llvm-$LLVM_VERSION
 LLVM_HOME=~/opt/llvm-$LLVM_VERSION
